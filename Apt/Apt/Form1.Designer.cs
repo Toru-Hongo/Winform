fa1ebb604c695b8514e5fba8353c559d97ee79d3
Toru-Hongo <hongo.toru@gmail.com>
2021-11-03 20:22:05 +0900

    ListBox 追加、関数TCalc 追加

--- Apt/Apt/Form1.Designer.cs
+++ Apt/Apt/Form1.Designer.cs
@@ -32,12 +32,15 @@ namespace Apt
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(250, 91);
+            this.button1.Location = new System.Drawing.Point(250, 114);
             this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(84, 34);
@@ -60,15 +63,47 @@ namespace Apt
             // 
             this.textBox1.Location = new System.Drawing.Point(26, 44);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(297, 19);
+            this.textBox1.Size = new System.Drawing.Size(126, 19);
             this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(191, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(126, 19);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 18);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(26, 83);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(204, 64);
+            this.listBox1.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(343, 134);
+            this.ClientSize = new System.Drawing.Size(343, 157);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
@@ -85,6 +120,9 @@ namespace Apt
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Apt/Apt/Form1.cs
+++ Apt/Apt/Form1.cs
@@ -12,6 +12,20 @@ namespace Apt
 {
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// Add
+        /// </summary>
+        /// <param name="x">param1</param>
+        /// <param name="y">param2</param>
+        /// <returns></returns>
+        public int Tcalc(int x,int y)
+        {
+            return x + y;
+        }
+
+        /// <summary>
+        /// init.
+        /// </summary>
         public Form1()
         {
             InitializeComponent();
@@ -20,12 +34,28 @@ namespace Apt
         private void Form1_Load(object sender, EventArgs e)
         {
             button1.Text = "OK";
-        }
+            textBox1.Text = "12";
+            textBox2.Text = "34";
+            listBox1.Items.Clear();
 
+        }
+        /// <summary>
+        /// Calc
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
             label1.Text = "Well come Git";
-            textBox1.Text = "";
+
+            int a, b,c;
+            a = int.Parse(textBox1.Text);
+            b = int.Parse(textBox2.Text);
+
+            c = Tcalc(a, b);
+
+            listBox1.Items.Add("Calc: " + c.ToString());
+
 
         }
     }
